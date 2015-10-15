5dd275b39b10767a878af1e6084db7a4d02b3cd0
Eunice <omgunis@gmail.com>
2015-10-14 19:54:11 -0700

    working

--- main.js
+++ main.js
@@ -3,40 +3,50 @@ $(function(){
   var ctx = canvas.getContext("2d");
   var w = canvas.width;
   var h = canvas.height;
-  var FPS = 100; //snakeArray moves every update, not speed
+  var FPS = 60; //snakeArray moves every update, not speed
   var speed = 1; //if speed is more than one, spaces are created between snakeArray
-  var score = 0;
+  var score;
   var twoPlayer = false;
   var direction1;//default direction
-  var direction2 = 'down';
+  var direction2;
   var update;
   var snakeArray; //array of cells that make the snake
   var snakeArray2;
   var food;
-  var textPos = 0;
 
-  function start(){
+
+  function menu(){
+    var textPos = 0;
     var animText = setInterval(textAnim, 60);
-      //SNAKE animation
+      //sexy SNAKE animation
       function textAnim(){
         clear();
         textPos += 10;
-        if(textPos > 150){
+        if(textPos > 160){
           clearInterval(animText);
         }
         ctx.font = "800 30px Arial";
         ctx.fillStyle = "tomato";
         ctx.fillText("SNAKE",250,textPos);
 
-        ctx.fillStyle = "tomato";
+        //player 1 temp box
+        ctx.fillStyle = "lightblue";
         var single =  new rect(200, 180, 100,25);
 
-        ctx.fillStyle = "#3366FF";
+        ctx.font = "300 15px Arial";
+        ctx.fillStyle = "tomato";
+        ctx.fillText("Single Player", 205, 200);
+
+        //player 2 temp box
+        ctx.fillStyle = "lightblue";
         var double =  new rect(310, 180, 100,25);
+
+        ctx.font = "300 15px Arial";
+        ctx.fillStyle = "#3366FF";
+        ctx.fillText("Two Player", 315, 200);
       }
 
-
-    //var singlePlayer = document.querySelector("#singlePlayer");
+    //initiates single or two player onClick
     canvas.addEventListener('click', function(evt){
       if(evt.x > 190 && evt.x < 300 && evt.y > 180 && evt.y < 215){
         twoPlayer = false;
@@ -49,15 +59,30 @@ $(function(){
         clearInterval(animText);
       }
     });
-    // twoPlayer = document.querySelector("#twoPlayer");
-    // twoPlayer.addEventListener('click', function(){
-    //   twoPlayer = true;
-    //   twoPlayerinit();
-    //   clearInterval(animText);
-    // });
   }
-  start();
-  function create_snake1(){
+
+  menu();
+
+  //runs if single player is selected in start
+  function singlePlayerinit() {
+    clear();
+    createSnake1();
+    createFood();
+    direction1 = 'up';
+    score = 0;
+    return update = setInterval(draw, FPS);
+  }
+  //runs if two player is selected in start
+  function twoPlayerinit() {
+    clear();
+    createSnake1();
+    createSnake2();
+    createFood();
+    direction1 = 'up';
+    direction2 = 'down';
+    return update = setInterval(draw, FPS);
+  }
+  function createSnake1(){
     var length = 5; //initial length of snake
     snakeArray = []; //start with empty array
     for (var i=0; i <= length; i++){
@@ -66,30 +91,13 @@ $(function(){
     }
   }
 
-  function create_snake2(){
+  function createSnake2(){
     var length = 5; //initial length of snake
     snakeArray2 = []; //start with empty array
     for (var i=length-1; i >= 0 ; i--){
       //creates a vertical snake moving down
       snakeArray2.push({x:20, y:i+10});
     }
-  }
-  //runs if single player is selected in start
-  function singlePlayerinit() {
-    clear();
-    create_snake1();
-    createFood();
-    direction1 = 'up';
-    score = 0;
-    return update = setInterval(draw, FPS);//updates screen every 10 milliseconds creating illusion of movement
-  }
-  //runs if two player is selected in start
-  function twoPlayerinit() {
-    clear();
-    createFood();
-    direction1 = 'up';
-    direction2 = 'up';
-    return update = setInterval(draw, FPS);//updates screen every 10 milliseconds creating illusion of movement
   }
   //Creates rectangles
   function rect(x,y,w,h) {
@@ -124,7 +132,6 @@ $(function(){
 
   function draw(){
     clear();
-    ctx.clearRect(0,0, w, h);
 
     //draws player 1
     for (var i=0; i < snakeArray.length; i++){
@@ -186,51 +193,62 @@ $(function(){
       tail.y = ny;
     }
 
-
     snakeArray.unshift(tail); //puts cell back in the front
     //tail becomes new head every frame
 
-    //player 2 draw
+    //draw Player 2
     if(twoPlayer == true){
-      ctx.fillStyle = player2.color;
-      rect(player2.x, player2.y, 10,10);
-    }
+      for (var i=0; i < snakeArray2.length; i++){
+        var c = snakeArray2[i];
+        ctx.fillStyle = 'tomato';
+        rect(c.x*10, c.y*10, 10, 10);
+      }
+      var nx2 = snakeArray2[0].x;
+      var ny2 = snakeArray2[0].y;
 
-    //player 2 collect food
-    if(twoPlayer ==true){
-      if(player2.x <= food.x + 10 && player2.x >= food.x - 10&&
-         player2.y <= food.y + 10 && player2.y >= food.y - 10){
+      //player 2 movement
+      if (direction2 == 'left') {
+        nx2 -= speed;
+      }
+      if (direction2 == 'up') {
+        ny2 -= speed;
+      }
+      if (direction2 == 'right') {
+        nx2 += speed;
+      }
+      if (direction2 == 'down') {
+        ny2 += speed;
+      }
+
+      //player2 collision
+      if (nx2 >= w/10 || nx2 < 0 || ny2 >= h/10 || ny2 < 0 || checkCollision(nx2, ny2, snakeArray2)){
+        console.log("DEAD");
+        clearInterval(update); //stops animation
+        alert("RED WINS!");
+      }
+
+      //clear food if touching player
+      //also had to divide food position by 10...
+      if(nx2 == food.x/10 && ny2 == food.y/10){
+        var tail2 = {
+          x: nx2,
+          y: ny2
+        }
         createFood();
         console.log('touch');
       }
-    }
+      else {
+        //x and y positions are updated every frame, but cells are not//
+        var tail2 = snakeArray2.pop(); //pops out last cell
+        tail2.x = nx2;
+        tail2.y = ny2;
+      }
 
-    //player2 collision
-    if(twoPlayer == true){
-      if (player2.x > w - 10 || player2.x < 1 || player2.y > h - 10 || player2.y < 1){
-        console.log("DEAD");
-        clearInterval(update); //stops animation
-        alert('RED WINS!');
-      }
-    }
-
-    //player 2 movement
-    if (twoPlayer == true){
-      if (direction2 == 'left') {
-        player2.x -= speed;
-      }
-      if (direction2 == 'up') {
-        player2.y -= speed;
-      }
-      if (direction2 == 'right') {
-        player2.x += speed;
-      }
-      if (direction2 == 'down') {
-        player2.y += speed;
-      }
+      //puts cell back in the front
+      //tail becomes new head every frame
+      snakeArray2.unshift(tail2);
     }
   }
-
   //player 1 keyboard input
   $(document).keydown(function(evt) {
     if (evt.keyCode == 37 && direction1 != 'right'){
@@ -250,19 +268,18 @@ $(function(){
   //gets player 2 keyboard input
   if(twoPlayer == true){
     $(document).keydown(function(evt) {
-      if (evt.keyCode == 65){
+      if (evt.keyCode == 65 && direction2 != 'right'){
         direction2 = 'left';
       }
-      else if (evt.keyCode == 87){
+      else if (evt.keyCode == 87 && direction2 != 'down'){
         direction2 = 'up';
       }
-      else if (evt.keyCode == 68){
+      else if (evt.keyCode == 68 && direction2 != 'left'){
         direction2 = 'right';
-
       }
-      else if (evt.keyCode == 83){
+      else if (evt.keyCode == 83 && direction2 != 'down'){
         direction2 = 'down';
       }
-    });
+    })
   }
 }) //end of iffe

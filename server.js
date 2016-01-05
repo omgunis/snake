1cfab87e701e02033be60b25e9571a83af214139
Eunice <omgunis@gmail.com>
2016-01-04 18:07:10 -0800

    added public folder

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var express    = require('express'),
     path       = require('path'),
     port       = process.env.PORT || 3000
 
+app.use(logger('dev'))
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res){
+  console.log('Sending index')
+  res.sendFile(__dirname + 'index.html')
+})
+
 app.listen(port, function(){
   console.log('Listening on port ' + port);
 })
